fd8268436deb453737adfa4c46dffb241ad8f3e6
Matheus Santos de Souza <matheus.santos.souza2020@outlook.com>
2023-03-15 21:32:29 -0400

    Test Data Builder

--- modulo07-DesignPatterns/creational/02.abstract-factory/platforms/browser/table.mjs
+++ modulo07-DesignPatterns/creational/02.abstract-factory/platforms/browser/table.mjs
@@ -2,6 +2,31 @@ import TableComponent from "../../shared/base/tableComponent.mjs";
 
 export default class TableBrowserComponent extends TableComponent {
     render(data) {
-        window.alert('chamou!!')
+        const template = this.prepareData(data)
+        document.body.insertAdjacentHTML('afterbegin', template)
+    }
+
+    prepareData(data) {
+        const [ firstItem ] = data
+        const tHeaders = Object.keys(firstItem)
+                    .map(text => `<th scope=col>${text}</th>`)
+        
+        const joinList = list => list.join('')
+        const tBodyValue = data
+                    .map(item => Object.values(item))
+                    .map(item => item.map(value => `<td>${value}</td>`))
+                    .map(tds => `<tr>${joinList(tds)}</tr>`)
+
+        const template = `
+        <table class="table">
+            <thead>
+                <tr>${joinList(tHeaders)}</tr>
+            </thead>
+            <tbody>
+                ${joinList(tBodyValue)}
+            </tbody>
+        </table>
+        `
+        return template
     }
 }

51a5d0cf0d83cc01bdbfbdabaa425c5846cd7d80
Matheus Santos de Souza <matheus.santos.souza2020@outlook.com>
2023-04-03 20:20:07 -0400

    npm versions

---
+++ modulo08-NPM/02.npm-package-versions/myapp/index.js
@@ -0,0 +1,14 @@
+import FluentSQLBuilder from '@matheus-santos-souza/fluentsql'
+
+import database from './database/data.json' assert { type: 'json' }
+
+const result = FluentSQLBuilder.for(database)
+                            .where({ registered: /^(2020|2019)/ })
+                            .select(['category'])
+                            .limit(3)
+                            .countBy('category')
+                            .build()
+                            
+                            
+                            
+console.log(result)
